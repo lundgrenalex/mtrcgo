71b74d16cc769c0c1db74608a844bbea24cc2b2e
Alex Lundgrenx <alex@endem.su>
2020-12-02 14:59:06 +0300

    Additional test

--- metrics/methods_test.go
+++ metrics/methods_test.go
@@ -7,6 +7,26 @@ func TestMetricExpose(t *testing.T) {
 	metric := SimpleMetric{
 		Name:  "test_metric",
 		Value: 45,
+        Labels: map[string]string{
+            "rsc_metric": "3711",
+        },
+		Date:  1606907901,
+	}
+
+	want := "test_metric{rsc_metric=\"3711\"} 45.000000\n"
+	res := Expose(MetricsSlice{metric})
+
+	if want != res {
+		t.Errorf("Expose format was incorrect!")
+	}
+
+}
+
+func TestMetricExposeWithEmptyLabels(t *testing.T) {
+
+    metric := SimpleMetric{
+		Name:  "test_metric",
+		Value: 45,
 		Date:  1606907901,
 	}
 
