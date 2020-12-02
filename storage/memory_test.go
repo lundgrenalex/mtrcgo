45804ba6885fbd841478ee032d0bcd4dda3ff95b
deepdishks <deepdishks@protonmail.com>
2020-12-02 21:46:19 +0300

    concurrent storage test case

--- storage/memory_test.go
+++ storage/memory_test.go
@@ -1,8 +1,10 @@
 package storage
 
 import (
-    "testing"
-    "github.com/lundgrenalex/mtrcgo/metrics"
+	"fmt"
+	"github.com/lundgrenalex/mtrcgo/metrics"
+	"sync"
+	"testing"
 )
 
 func TestStorage(t *testing.T) {
@@ -48,4 +50,49 @@ func TestStorage(t *testing.T) {
 		t.Errorf("I can't dump metric from store!")
 	}
 
+}
+
+func TestConcurrentStorage(t *testing.T) {
+	metricsToRemove := 499
+	metricsToCreate := 5000
+	var wg sync.WaitGroup
+
+	s := Init()
+	if s == nil {
+		t.Errorf("I can't init storage!")
+	}
+	
+	m := make(metrics.MetricsSlice, metricsToCreate)
+	for i := 0; i < metricsToCreate; i++ {
+		m[i] = metrics.SimpleMetric{
+			Name:  "test_metric",
+			Value: float32(i),
+			Labels: map[string]string{
+				fmt.Sprintf("%d", i): "3711",
+			},
+			Date: 1606907901,
+		}
+	}
+
+	wg.Add(len(m))
+	for _, mtrc := range m {
+		go func(i metrics.SimpleMetric) {
+			defer wg.Done()
+			s.Upsert(i)
+		}(mtrc)
+	}
+	wg.Wait()
+
+	wg.Add(metricsToRemove)
+
+	for i := 0; i < metricsToRemove; i ++ {
+		go func(idx int) {
+			defer wg.Done()
+			s.Remove(m[idx+idx])
+		}(i)
+	}
+	wg.Wait()
+	if len(s.Dump()) != metricsToCreate - metricsToRemove {
+		t.Errorf("Upserted metrics count != removed metrics count")
+	}
 }
