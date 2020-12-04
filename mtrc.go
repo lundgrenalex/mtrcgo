d96553159811ae1ce13090f50bb3c1b66a6e2701
Alex Lundgrenx <alex@endem.su>
2020-12-04 19:21:19 +0300

    Added go.mod, added yaml support

--- mtrc.go
+++ mtrc.go
@@ -1,16 +1,96 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/lundgrenalex/mtrcgo/api"
 	"github.com/lundgrenalex/mtrcgo/storage"
+	"gopkg.in/yaml.v2"
 )
 
-const addr = "127.0.0.1:8080"
+// Config struct
+type Config struct {
+	Server struct {
+		// Host is the local machine IP Address to bind the HTTP Server to
+		Host string `yaml:"host"`
+
+		// Port is the local machine TCP Port to bind the HTTP Server to
+		Port string `yaml:"port"`
+	} `yaml:"server"`
+}
+
+// NewConfig returns a new decoded Config struct
+func NewConfig(configPath string) (*Config, error) {
+	// Create config structure
+	config := &Config{}
+
+	// Open config file
+	file, err := os.Open(configPath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	// Init new YAML decode
+	d := yaml.NewDecoder(file)
+
+	// Start YAML decoding from file
+	if err := d.Decode(&config); err != nil {
+		return nil, err
+	}
+
+	return config, nil
+}
+
+// ValidateConfigPath just makes sure, that the path provided is a file,
+// that can be read
+func ValidateConfigPath(path string) error {
+	s, err := os.Stat(path)
+	if err != nil {
+		return err
+	}
+	if s.IsDir() {
+		return fmt.Errorf("'%s' is a directory, not a normal file", path)
+	}
+	return nil
+}
+
+// ParseFlags will create and parse the CLI flags
+// and return the path to be used elsewhere
+func ParseFlags() (string, error) {
+	// String that contains the configured configuration path
+	var configPath string
+
+	// Set up a CLI flag called "-config" to allow users
+	// to supply the configuration file
+	flag.StringVar(&configPath, "config", "./config.yml", "path to config file")
+
+	// Actually parse the flags
+	flag.Parse()
+
+	// Validate the path first
+	if err := ValidateConfigPath(configPath); err != nil {
+		return "", err
+	}
+
+	// Return the configuration path
+	return configPath, nil
+}
 
 func main() {
+
+	cfgPath, err := ParseFlags()
+	if err != nil {
+		log.Fatal(err)
+	}
+	config, err := NewConfig(cfgPath)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	s := storage.Init()
 
@@ -21,6 +101,8 @@ func main() {
 	http.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
 		api.ExposeMetrics(s, w, r)
 	})
+
+	addr := config.Server.Host + ":" + config.Server.Port
 
 	log.Println("Server listening on: " + addr)
 	log.Fatal(http.ListenAndServe(addr, nil))
